52865c4e49621fdb7574bb34b2a02c731eb44331
magaev <magomed.agaev@laplateforme.io>
2023-10-13 11:23:28 +0200

    Job06

---
+++ Job06/my_calculator.sh
@@ -0,0 +1,13 @@
+if [ $2 = "+" ]
+then
+echo resultat=$(($1+$3))
+elif [ $2 = "-" ]
+then
+echo resultat=$(($1-$3))
+elif [ $2 = "x" ]
+then
+echo resultat=$(($1*$3))
+elif [ $2 = "/" ]
+then
+echo resultat=$(($1/$3))
+fi
